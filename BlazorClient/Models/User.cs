d2bbb3f106fba0c8d1cf28d43fee7ac554d51394
143473 <75113646+143473@users.noreply.github.com>
2021-11-28 16:23:36 +0100

    fixed few user error messages for inputs

--- BlazorClient/Models/User.cs
+++ BlazorClient/Models/User.cs
@@ -7,13 +7,11 @@ namespace BlazorClient.Models
     {
 
         public int Id { get; set; }
-        
 
+        [Required, MaxLength(15, ErrorMessage = "Username too long")]
         public string UserName { get; set; }
-        
-
+        [Required, MinLength(4, ErrorMessage = "Password too short, minimum 4 characters")]
         public string Password { get; set; }
-        
 
         public string Role { get; set; }
         
